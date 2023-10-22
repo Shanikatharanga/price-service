dc47e29cc34ca35012caaf9cdeaff87ebb79c358
Shanika Tharanga <shanikatharanga@gmail.com>
2023-10-22 20:26:04 +0530

    price service edited

--- src/main/java/com/priceservice/priceservice/PriceController.java
+++ src/main/java/com/priceservice/priceservice/PriceController.java
@@ -3,30 +3,25 @@ package com.priceservice.priceservice;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class PriceController {
 		
 		List<Price> priceList = new ArrayList<Price>();
 		
-		@Autowired
-		RestTemplate restTemplate;
+		WebClient webClient = WebClient.create();
 
 		@GetMapping("/price/details/{productId}")
-		public Price getPriceDetails(@PathVariable("productId") Long productid) {
+		public Mono<Price> getPriceDetails(@PathVariable("productId") Long productid) {
 			
-			Price price = getPrice(productid);
+			Mono<Price> price = Mono.just(getPrice(productid));
 			
-			Integer excVal = restTemplate
-					.getForObject("http://localhost:8004/exchange/from/USD/to/YEN", ExcVal.class)
-					.getExVal();
-			
-			price.setDiscountPrice(Math.multiplyExact(excVal, price.getDiscountPrice()));
 			
 			return price;
 		}
@@ -42,6 +37,7 @@ public class PriceController {
 		}
 
 		private void populatePriceList() {
+			priceList.clear();
 			priceList.add(new Price(201L,101L,1999,999));
 			priceList.add(new Price(202L,102L,199,99));
 			priceList.add(new Price(203L,103L,1999,699));
